d5a2dbf0f2d4d91eda142f270395db3b139abeac
Byron Lutz <byronlutz@mail.weber.edu>
2024-05-17 15:54:52 -0600

    Initial Commit

---
+++ Hello World/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Hello World
+//
+//  Created by Byron Lutz on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
